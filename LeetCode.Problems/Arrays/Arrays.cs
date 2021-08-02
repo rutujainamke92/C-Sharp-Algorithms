0f7a2775f2d14c17706c66ab9851de385b34233e
ChetanPachpande <chetandpachpande@gmail.com>
2021-08-02 00:04:20 -0500

    Solved LeetCode Problem no 01

---
+++ LeetCode.Problems/Arrays/Arrays.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems.Arrays
+{
+    public static class Arrays
+    {
+        /// <summary>
+        /// https://leetcode.com/problems/two-sum/
+        /// 1. Two Sum
+        /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        /// You can return the answer in any order.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            //define array of size 2 to store result
+            int[] result = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    // Skip if you are looking at the same number from the input array
+                    if (i != j)
+                    {
+                        if (nums[i] + nums[j] == target)
+                        {
+                            result[0] = i;
+                            result[1] = j;
+                            return result;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// https://leetcode.com/problems/two-sum/
+        /// 1. Two Sum
+        /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+        /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+        /// You can return the answer in any order.
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum1(int[] nums, int target)
+        {
+            //define array of size 2 to store result
+            int[] result = new int[2];
+                        
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(map.ContainsKey(target - nums[i]))
+                {
+                    if(map.ContainsKey(target - nums[i]))
+                        return new int[] { map[target - nums[i]], i };
+                }
+                map.Add(nums[i], i);            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTest/LeetCode.Problems.Tests/ArraysTests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode.Problems.Arrays;
+using Xunit;
+
+namespace UnitTest.LeetCode.Problems.Tests
+{
+    public static class ArraysTests
+    {
+        [Fact]
+        public static void TestProblems()
+        {
+            //Test TwoSum
+            // LeetCode :: 1. Two Sum
+            // https://leetcode.com/problems/two-sum/
+            int[] nums = { 2, 7, 15, 11 };
+            const int target = 9;
+            int[] expectedResult = { 0, 1 };
+
+            var result = Arrays.TwoSum(nums, target);
+            Assert.True(System.Linq.Enumerable.SequenceEqual(expectedResult, result));
+
+            var result1 = Arrays.TwoSum1(nums, target);
+            Assert.True(System.Linq.Enumerable.SequenceEqual(expectedResult, result1));
+        }
+    }
+}
